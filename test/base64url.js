b725730463f1c6c2b342c38305f28f5aee921ddd
Gustavo Leguizamon <leguizamon.gustavo@gmail.com>
2013-08-19 23:16:15 -0300

    Refactor styling

--- test/base64url.js
+++ test/base64url.js
@@ -2,29 +2,29 @@
  * Module dependencies.
  */
 
-var base64url = require('../lib/base64url')
+var base64url = require('../lib/base64url');
 
 /**
  * Tests.
  */
 
-describe('.urlSafe(str)', function() {
-  it('should return the url safe version of base64-encoded str', function() {
-    var val = base64url.urlSafe('a+b/c===')
-    val.should.equal('a-b_c')
-  })
-})
+describe('.urlSafe(str)', function(){
+  it('should return the url safe version of base64-encoded str', function(){
+    var val = base64url.urlSafe('a+b/c===');
+    val.should.equal('a-b_c');
+  });
+});
 
-describe('.urlSafeEncode(str)', function() {
-  it('should return the url safe base64-encoded version of str', function() {
-    var val = base64url.urlSafeEncode('irbis?lab?')
-    val.should.equal('aXJiaXM_bGFiPw')
-  })
-})
+describe('.urlSafeEncode(str)', function(){
+  it('should return the url safe base64-encoded version of str', function(){
+    var val = base64url.urlSafeEncode('irbis?lab?');
+    val.should.equal('aXJiaXM_bGFiPw');
+  });
+});
 
-describe('.urlSafeDecode(str)', function() {
-  it('should return the base64-decoded version of str', function() {
-    var val = base64url.urlSafeDecode('aXJiaXM_bGFiPw')
-    val.should.equal('irbis?lab?')
-  })
-})
+describe('.urlSafeDecode(str)', function(){
+  it('should return the base64-decoded version of str', function(){
+    var val = base64url.urlSafeDecode('aXJiaXM_bGFiPw');
+    val.should.equal('irbis?lab?');
+  });
+});
--- test/signing.js
+++ test/signing.js
@@ -2,30 +2,30 @@
  * Module dependencies.
  */
 
-var signing = require('../lib/signing')
+var signing = require('../lib/signing');
 
 /**
  * Tests.
  */
 
-describe('.sign(value, secret, salt)', function() {
-  it('should sign the value', function() {
-    var val = signing.sign('hi', 'secret', 'salt')
-    val.should.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA')
+describe('.sign(value, secret, salt)', function(){
+  it('should sign the value', function(){
+    var val = signing.sign('hi', 'secret', 'salt');
+    val.should.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA');
 
-    val = signing.sign('hi', 'othersecret', 'salt')
-    val.should.not.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA')
+    val = signing.sign('hi', 'othersecret', 'salt');
+    val.should.not.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA');
 
-    val = signing.sign('hi', 'secret', 'othersalt')
-    val.should.not.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA')
-  })
-})
+    val = signing.sign('hi', 'secret', 'othersalt');
+    val.should.not.equal('aGk.fYUxykx5WU-pHxHRIWkQrS4alndcr17dx3RZ5BSGiOA');
+  });
+});
 
-describe('.unsign(signed, secret, salt)', function() {
-  it('should unsign the signed value', function() {
-    var val = signing.sign('hi', 'secret', 'salt')
-    signing.unsign(val, 'secret', 'salt').should.equal('hi')
-    signing.unsign(val, 'othersecret', 'salt').should.be.false
-    signing.unsign(val, 'secret', 'othersalt').should.be.false
-  })
-})
+describe('.unsign(signed, secret, salt)', function(){
+  it('should unsign the signed value', function(){
+    var val = signing.sign('hi', 'secret', 'salt');
+    signing.unsign(val, 'secret', 'salt').should.equal('hi');
+    signing.unsign(val, 'othersecret', 'salt').should.be.false;
+    signing.unsign(val, 'secret', 'othersalt').should.be.false;
+  });
+});
